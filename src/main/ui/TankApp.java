3351b4e866d95dcad72d1b71726cc5f65e2d0ac1
eric <ericlu133@gmail.com>
2022-07-23 21:41:04 -0700

    tests are complete, pre-implementation commit

--- src/main/ui/TankApp.java
+++ src/main/ui/TankApp.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TankApp {
-    public static final int MAX_FISH = 5;
+    public static final int MAX_FISH = 2;
     public static final int DECAY_RATE_PER_SECOND = 5;
 
     private ArrayList<Fish> tank;
@@ -24,25 +24,58 @@ public class TankApp {
 
     // MODIFIES: this
     // EFFECTS: attempts to add the given Fish to the tank, if there's space, returns true, else false
-    private boolean addFish(Fish fish) {
+    public boolean addFish(Fish fish) {
         return true;
     }
 
     // MODIFIES: this
     // EFFECTS: attempts to remove the Fish with the corresponding given fishName, if succeeds returns true, else false
-    private boolean removeFish(String fishName) {
+    public boolean removeFish(String fishName) {
         return true;
+    }
+
+    // EFFECTS: returns the Fish object with the given name, if not found, returns null
+    public Fish getFishFromName(String fishName) {
+        return null;
+    }
+
+    // REQUIRES: index is non-negative
+    // EFFECTS: returns the fish in the given index of the list
+    public Fish getFish(int fishIndex) {
+        return null;
     }
 
     // REQUIRES: tank is non-empty
     // MODIFIES: this
     // EFFECTS: feeds all the fish inside the tank ArrayList
-    private void feedAllFish() {
+    public void feedAllFish() {
+
+    }
+
+    // REQUIRES: tank is non-empty
+    // MODIFIES: this
+    // EFFECTS: reduces the hunger values of all fish in the tank by 1
+    public void hungerAllFish() {
 
     }
 
     // EFFECTS: returns a string of all the Fish in the tank with their respective attributes
-    private String listFish() {
+    public String listFish() {
         return "";
+    }
+
+    // EFFECTS: returns the number of fish in the tank
+    public int size() {
+        return tank.size();
+    }
+
+    // EFFECTS: returns true if the tank has no fish, else false
+    public boolean isEmpty() {
+        return (tank.size() == 0);
+    }
+
+    // EFFECTS: returns true if the tank size is equal to MAX_FISH, else false
+    public boolean isFull() {
+        return (tank.size() == MAX_FISH);
     }
 }
--- src/test/model/FishTest.java
+++ src/test/model/FishTest.java
@@ -2,19 +2,100 @@ package model;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import ui.TankApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ui.TankApp.MAX_FISH;
 
 class FishTest {
     // delete or rename this class!
 
-    TankApp tank;
+    TankApp tankApp;
     Fish fish1;
     Fish fish2;
 
     @BeforeEach
     public void setup() {
-
+        fish1 = new Fish("Dory","blue","Blue tang");
+        fish2 = new Fish("Nemo","Orange","Goldfish");
+        tankApp = new TankApp();
     }
+
+    @Test
+    public void testFish() {
+        assertEquals(fish1.getName(), "Dory");
+        assertEquals(fish2.getColor(), "Orange");
+        assertEquals(fish2.getSpecies(), "Goldfish");
+    }
+
+    @Test
+    public void testDecreaseHunger() {
+        assertEquals(fish1.getHunger(), 100);
+        fish1.decreaseHunger();
+        assertEquals(fish1.getHunger(), 99);
+        fish1.decreaseHunger();
+        assertEquals(fish1.getHunger(), 98);
+        fish2.decreaseHunger();
+        assertEquals(fish2.getHunger(), 99);
+    }
+
+    @Test
+    public void testFeed() {
+        fish1.decreaseHunger();
+        fish1.decreaseHunger();
+        assertEquals(fish1.getHunger(), 98);
+        fish1.feed();
+        assertEquals(fish1.getHunger(), 100);
+    }
+
+    @Test
+    public void testStartTank() {
+        assertTrue(tankApp.isEmpty());
+    }
+
+    @Test
+    public void testAddRemoveFish() {
+        tankApp.addFish(fish1);
+        tankApp.addFish(fish2);
+        assertEquals(tankApp.getFish(0), fish1);
+        assertEquals(tankApp.getFish(1), fish2);
+        tankApp.removeFish("Nemo");
+        assertEquals(tankApp.size(), 1);
+        assertEquals(tankApp.getFishFromName("Dory"), fish1);
+    }
+
+    @Test
+    public void testIsFull() {
+        for (int i = 0; i < MAX_FISH; i++) {
+            tankApp.addFish(fish1);
+        }
+        assertTrue(tankApp.isFull());
+    }
+
+    @Test
+    public void testHungerAllFeedAll() {
+        tankApp.addFish(fish1);
+        tankApp.addFish(fish2);
+        tankApp.hungerAllFish();
+        assertEquals(fish1.getHunger(), 99);
+        assertEquals(fish2.getHunger(), 99);
+        tankApp.hungerAllFish();
+        assertEquals(fish1.getHunger(), 98);
+        assertEquals(fish2.getHunger(), 98);
+        tankApp.feedAllFish();
+        assertEquals(fish1.getHunger(), 100);
+        assertEquals(fish2.getHunger(), 100);
+    }
+
+    @Test
+    public void testGetSummary() {
+        assertEquals(fish1.getSummary(), "Dory is a blue Blue Tang.");
+    }
+
+
+
+
 
 
 }
